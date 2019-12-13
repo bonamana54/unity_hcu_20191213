a72ed0292e6d216da1825ffac29b6d82199d9d2c
bonamana54 <bomanama54@gmail.com>
2019-12-13 15:05:13 +0800

    子彈飛

--- 20191213/Assets/spicts/player.cs
+++ 20191213/Assets/spicts/player.cs
@@ -3,14 +3,50 @@
 public class player : MonoBehaviour 
 {
 	#region 欄位
+
+    public float speedBullet = 1000;
      [Header("移動速度"),Range(0,2000)]
      public float speed = 1.5f;
 
 	  [Header("血量"),Range(100,1000)]
      public float hp = 100;
+     public GameObject bullte;
+     public AudioClip soundFire;
+     public Transform point;
 
-	 public Animator ani;
-	 public Rigidbody2D r2d;
+	 private Animator ani;
+	 private Rigidbody2D r2d;
+     private AudioSource aud ;
 
     #endregion 
+
+    private void Start() {
+        {
+            ani = GetComponent<Animator>();
+            r2d = GetComponent<Rigidbody2D>();
+            aud = GetComponent<AudioSource>();
+        }
+    }
+    private void FixedUpdate(){
+        Move();
+    }
+
+    private void Update(){
+        Fire();
+    }
+    private void Move(){
+
+        float h  = Input.GetAxisRaw("Horizontal");
+        r2d.AddForce(new Vector2(speed*h,0));
+        if (h != 0) transform.localScale = new Vector3(h,1,1);
+    }
+
+    private void Fire(){
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            aud.PlayOneShot(soundFire);
+            GameObject temp = Instantiate(bullte,point.position,Quaternion.identity);
+            temp.GetComponent<Rigidbody2D>().AddForce(new Vector2(speedBullet*transform.localScale.x,0));
+        }
+    }
+
 }
